94dd68fe652cb72248f62865f8e1abd311aab25d
alex-couchman <alexander.couchman@outlook.com>
2025-11-01 22:11:06 -0400

    refactored

--- CH4L10/main.py
+++ CH4L10/main.py
@@ -5,3 +5,18 @@ def insertion_sort(nums):
             nums[j-1], nums[j] = nums[j], nums[j-1]
             j -= 1
     return nums
+
+def insertion_sort(nums):
+    count = 0
+    for j in range (1, len(nums)):
+        while j > 0 and nums[j-1] > nums[j]:
+            nums[j-1], nums[j] = nums[j], nums[j-1]
+            j -= 1
+            count += 1
+    return nums, count
+
+if __name__ == "__main__":
+    nums = [0, -2, -5, 3, 2, 1]
+    result, iter = insertion_sort(nums)
+    print(iter)
+    print(result)
