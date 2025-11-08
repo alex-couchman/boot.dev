9c61304a2e409bd825e466bf08e8386516cba3b4
alex-couchman <alexander.couchman@outlook.com>
2025-11-07 19:28:25 -0500

    completed CH8L6

--- CH8/L2/main.py
+++ CH8/L2/main.py
@@ -3,19 +3,19 @@ class Queue:
         self.items = []
 
     def push(self, item):
-        self.items.insert(0, item)
+        self.items.append(item)
 
     def pop(self):
         if self.size() == 0:
             return None
-        item = self.items[-1]
-        del self.items[-1]
+        item = self.items[0]
+        del self.items[0]
         return item
 
     def peek(self):
         if self.size() == 0:
             return None
-        return self.items[-1]
+        return self.items[0]
 
     def size(self):
         return len(self.items)
--- CH8/L6/main.py
+++ CH8/L6/main.py
@@ -5,8 +5,19 @@ def matchmake(queue, user):
     name, action = user[0], user[1]
     if action == "leave":
         queue.search_and_remove(name)
+    if action == "join":
+        queue.push(name)
+    if queue.size() >= 4:
+        user1 = queue.pop()
+        user2 = queue.pop()
+        return f"{user1} matched {user2}!"
+    else:
+        return "No match found"
+    
+
+
+
+
 
 if __name__ == "__main__":
-    queue = Queue()
-    queue.push
-
+    pass
