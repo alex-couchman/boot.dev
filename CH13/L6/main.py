09d7afdbf3b63c51b74e720cc296f80b200b2298
alex-couchman <alexander.couchman@outlook.com>
2025-11-15 16:50:36 -0500

    completed CH13 L6

--- CH13/L6/main.py
+++ CH13/L6/main.py
@@ -1,8 +1,20 @@
 class Trie:
     def find_matches(self, document):
         matches = set()
+        current = self.root
+        word = ""
+        for char in document:
+            if char in current:
+                word += char
+                current = current[char]
+                if self.end_symbol in current:
+                    matches.add(word)
+            else:
+                word = ""
+                current = self.root
+        return matches
+            
         
-
     # don't touch below this line
 
     def __init__(self):
