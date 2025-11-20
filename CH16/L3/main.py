da29e578c9457ecc7ff9a373bb4892176cc3408c
alex-couchman <alexander.couchman@outlook.com>
2025-11-19 21:20:40 -0500

    completed CH16 L3

---
+++ CH16/L3/main.py
@@ -0,0 +1,33 @@
+def tsp(cities, paths, dist):
+    perms = permutations(cities)
+    for perm in perms:
+        sum = 0
+        for i in range(len(perm)-1):
+            curr_city, next_city = perm[i], perm[i+1]
+            sum += paths[curr_city][next_city]
+        if sum < dist:
+            return True
+    return False
+
+
+# don't touch below this line
+
+
+def permutations(arr):
+    res = []
+    res = helper(res, arr, len(arr))
+    return res
+
+
+def helper(res, arr, n):
+    if n == 1:
+        tmp = arr.copy()
+        res.append(tmp)
+    else:
+        for i in range(n):
+            res = helper(res, arr, n - 1)
+            if n % 2 == 1:
+                arr[n - 1], arr[i] = arr[i], arr[n - 1]
+            else:
+                arr[0], arr[n - 1] = arr[n - 1], arr[0]
+    return res
